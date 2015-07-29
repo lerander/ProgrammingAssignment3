8f250564a4d8783894c1392ee1f521cd050608f4
Lars Erik Ribe Anderssen <git@lars-erik.com>
2015-07-29 14:52:13 +0200

    Some renaming of variables. Cleanup of comments.

--- rankhospital.R
+++ rankhospital.R
@@ -8,15 +8,15 @@ rankhospital <- function(state, outcome, num = "best") {
                              na.strings = "Not Available")
         
         ## Move data of interest into a frame
-        washed_file <- data.frame("name" = hospfile[, 2],
+        washed_content <- data.frame("name" = hospfile[, 2],
                                   "state" = hospfile[, 7],
                                   "heart attack" = hospfile[, 11],
                                   "heart failure" = hospfile[, 17],
                                   "pneumonia" = hospfile[, 23],
                                   stringsAsFactors = FALSE)
         
-        ## Let's move the rows that are in the relevant state to a smaller subset.
-        all_in_state <- washed_file[which(washed_file$state == state), ]
+        ## The rows that are in the relevant state are moved to a smaller subset.
+        all_in_state <- washed_content[which(washed_content$state == state), ]
         
         ## All states have hospitals. If no hospitals, the state was invalid.
         if(nrow(all_in_state) == 0) stop("Invalid state")
@@ -25,31 +25,32 @@ rankhospital <- function(state, outcome, num = "best") {
         ## If none of the predefined outcomes: the outcome was invalid.
         if (outcome == "heart attack") {
                 hospitals <- data.frame("name" = as.character(all_in_state$name), 
-                                        "death percentage" = all_in_state$heart.attack,
+                                        "outcome percentage" = all_in_state$heart.attack,
                                         stringsAsFactors = FALSE)
         } else if (outcome == "heart failure") {
                 hospitals <- data.frame("name" = as.character(all_in_state$name), 
-                                        "death percentage" = all_in_state$heart.failure,
+                                        "outcome percentage" = all_in_state$heart.failure,
                                         stringsAsFactors = FALSE)
         } else if (outcome == "pneumonia") {
                 hospitals <- data.frame("name" = as.character(all_in_state$name), 
-                                        "death percentage" = all_in_state$pneumonia,
+                                        "outcome percentage" = all_in_state$pneumonia,
                                         stringsAsFactors = FALSE)
         } else {
                 stop("Invalid outcome")
         }
         
-        ## Sort the frame by 1. death percentage, 2. name. Remove NAs.
-        sort_vector <- order(hospitals$death.percentage,
+        ## Sort the frame by 1. outcome percentage, 2. name. Remove NAs.
+        sort_vector <- order(hospitals$outcome.percentage,
                              hospitals$name,
                              na.last = NA)
         
         ## Handling of the special legal values for num
-        if(num == "worst") {
-                index <- length(sort_vector)
-        } else if(num == "best") {
+        if(num == "best") {
                 index <- 1
+        } else if(num == "worst") {
+                index <- length(sort_vector)
         } else index <- num
         
+        ## return name of the hospital with requested rank
         hospitals$name[sort_vector[index]]
 }
