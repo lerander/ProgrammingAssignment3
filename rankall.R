2c96cb536385bf6ab0a6badf5987912b677468df
Lars Erik Ribe Anderssen <git@lars-erik.com>
2015-08-04 13:45:32 +0200

    Quicker. Not very R-spesific code, though.

--- rankall.R
+++ rankall.R
@@ -1,11 +1,9 @@
 rankall <- function(outcome, num = "best") {
-        ## Read outcome data
-        ## Check that state and outcome are valid
-        ## For each state, find the hospital of the given rank
+        ## For each state, finds the hospital of the given rank
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
         
-        ## Defining columns, and identifying illegal outcome input.
+        ## Defining columns. Stopping if illegal outcome input.
         name_column <- 2
         state_column <- 7
         if (outcome == "heart attack")          outcome_column <- 11
@@ -13,63 +11,45 @@ rankall <- function(outcome, num = "best") {
         else if (outcome == "pneumonia")        outcome_column <- 23
         else stop("Invalid outcome")
         
-        hospfile <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+        hospfile <- read.csv("outcome-of-care-measures.csv",
+                             na.strings = "Not Available",
+                             stringsAsFactors = FALSE)
         
         ## Move data of interest into a frame
-        hospitals <- data.frame("name" = hospfile[, name_column],
-                                "state" = hospfile[, state_column],
-                                "outcome percentage" = hospfile[, outcome_column],
-                                stringsAsFactors = FALSE)
+        hospitals <- na.omit(data.frame("name" = hospfile[, name_column],
+                                        "state" = hospfile[, state_column],
+                                        "outcome" = hospfile[, outcome_column],
+                                        stringsAsFactors = FALSE))
         
-        completes <- hospitals[complete.cases(hospitals), ]
+        ## Order the data. Handle "worst"
+        hospitals <- hospitals[order(hospitals[, 2],
+                                     hospitals[, 3]*(1-2*(num == "worst")),
+                                     hospitals[, 1]),]
         
-        ## when worst, we reverse ordering for outcome, but not for the others.
-        if(num == "worst") {
-                sort_vector <- order(completes$state,
-                                     - completes$outcome.percentage,
-                                     completes$name)
-        } else {
-                sort_vector <- order(completes$state,
-                                     completes$outcome.percentage,
-                                     completes$name)
-        }
+        if(!is.numeric(num)) num <- 1 ## Best/worst is concidered as 1.
         
-        if(num == "worst" || num == "best") num <- 1
+        hospitalcount <- nrow(hospitals) ## To avoid counting several times later on.
+
         
-        ## return name of the hospital with requested rank
-        ordered_completes <- data.frame("state" = completes$state[sort_vector],
-                                        "name" = completes$name[sort_vector],
-                                        "outcome" = completes$outcome.percentage[sort_vector])
+        if(num < 1 || num > hospitalcount) stop("Illegal num")
         
-        ## Now we have an ordered frame containing complete cases of state, name and outcome
+        state = ""
+        states <- unique(hospitals$state)
         
-        ## Easy way to solve this, implemented below:
-        ## Loop from 1 to length of the frame
-        ## Look at the state. If it is "new", count (num - 1) from this. If state is still
-        ## the same: store to result. (If not, store state and NA)
-        state = "NN"
-        name = "Initial"
-        ## Had some data.frame-problems. therefore populate a matrix
-        ## first, and present as data.frame afterwards. Should be fixed.
-        ## Also: predefining the size is obviously a hack.
-        result <- matrix(nrow=54, ncol=2, byrow=TRUE, dimnames = list(NULL, c("hospital", "state")))
+        #Prepare result containing all the states.
+        result <- data.frame(hospital = NA,
+                             state = states,
+                             row.names = states)
         
-        counter <- 0
-        
-        # Should really find a more elegant way to do this!
-        for(stateindex in (1:nrow(ordered_completes))) {
-                if (ordered_completes[stateindex, 1] != state) { #First row with a new state.
-                        state <- ordered_completes[stateindex, 1]
-                        counter <- counter + 1
-                        if (ordered_completes[stateindex + num - 1, 1] == state && ## offset with num-1, and check if same state
-                            stateindex + num - 1 <= nrow(ordered_completes)) { ## Don't move beyond last entry
-                                result[counter, ] <- c(c(as.character(ordered_completes[stateindex + num - 1, 2]),
-                                                         as.character(state)))
-                        }
-                        else if (state != "NN") { ## Push NA and state to result. (The dummy value should not be pushed.)
-                                result[counter, ] <- c(NA, c(as.character(state)))
+        # This may not be so stupid after all. (Not spesific R code, however.)
+        for(i in (1:(hospitalcount - num + 1))) { ## Spin through data set.
+                if (hospitals$state[i] != state) { # Whenever a new state is reached ...
+                        state <- hospitals$state[i] ## We prepare for updating result for this state.
+                        if (hospitals$state[i + num - 1] == state) { ## Check that the offseted row is same state ...
+                                result[state, 1] <- hospitals$name[i + num - 1] # ... and update the result.
                         }
                 }
         }
-        print(as.data.frame(result))
+        
+        print(result)
 }
